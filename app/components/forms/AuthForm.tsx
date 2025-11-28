7335803e80fb251bb3232000d8e2c52ba5097b2f
samanhoseinpour <samangithoseinpour@gmail.com>
2025-11-28 18:26:49 +0330

    update login form

--- app/components/forms/AuthForm.tsx
+++ app/components/forms/AuthForm.tsx
@@ -1,19 +1,23 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { SocialAuthForm } from '../../components';
+import Link from "next/link";
+import { LoginForm } from "@/components/login-form";
+
+import { GalleryVerticalEnd } from "lucide-react";
 const AuthForm = () => {
   return (
-    <section className="mx-auto container flex-col justify-between items-center text-center bg-zinc-50 text-black w-2xl rounded-xl min-h-[50svh] py-12">
-      <Link href="/" className="flex justify-around items-center">
-        <div className="flex flex-col justify-start items-start">
-          <h1>Join AIOverflow</h1>
-          <p>to get your awnsered questions</p>
-        </div>
-        <Image src="/favicon.ico" alt="website logo" width={40} height={40} />
-      </Link>
-
-      <SocialAuthForm />
-    </section>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Ai Overflow.
+        </Link>
+        <LoginForm />
+      </div>
+    </div>
   );
 };
 
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,6 @@
-export { default as Navbar } from './navbar/Navbar';
-export { default as ThemeToggler } from './navbar/ThemeToggler';
+export { default as Navbar } from "./navbar/Navbar";
+export { default as ThemeToggler } from "./navbar/ThemeToggler";
 
-export { default as AuthForm } from './forms/AuthForm';
-export { default as SocialAuthForm } from './forms/SocialAuthForm';
+export { default as AuthForm } from "./forms/AuthForm";
 
-export { default as Container } from './Container';
+export { default as Container } from "./Container";
